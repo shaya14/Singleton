c9f204ab49d049a91c2f3d1371c1aac8588e2275
shaya14 <84465665+shaya14@users.noreply.github.com>
2021-11-14 19:05:46 +0200

    add

---
+++ Singleton Leasson/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton_Leasson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wallet = 50000;
+            while (true)
+            {
+
+
+                Console.WriteLine("Welcome to your bank account , You want to deposit or withdraw?");
+                string action = Console.ReadLine();
+                switch (action)
+                {
+                    case "deposit":
+                        Console.WriteLine("How much you want to deposit?");
+                        float deposit = float.Parse(Console.ReadLine());
+                        deposit += Singleton.Instance.Deposit(deposit);
+                        break;
+                    case "withdraw":
+                        Console.WriteLine("How much you want to withdraw?");
+                        float wittdraw = float.Parse(Console.ReadLine());
+                        wittdraw -= Singleton.Instance.WithDraw(wittdraw);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invaild input, please enter corrent answer");
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Singleton Leasson/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Leasson
+{
+    class Singleton
+    {
+        public static Singleton Instance;
+        float bankMoney;
+
+        private Singleton()
+        {
+            Instance = this;
+            bankMoney = 0;
+        }
+        public float Deposit(float amount)
+        {
+            if(amount > 0)
+            {
+                Console.WriteLine($"You deposit {amount} to your account!");
+                bankMoney += amount;
+                return amount;
+            }
+            else
+            {
+                Console.WriteLine("Invaild Amount!");
+                return 0;
+            }
+        }
+        public float WithDraw(float amount)
+        {
+            if(bankMoney - amount > 0)
+            {
+                Console.WriteLine($"You withdraw {amount} from your account , You have {bankMoney} now");
+                bankMoney -= amount;
+                return amount;
+            }
+            else
+                Console.WriteLine("Invaild Amount!");
+            return 0;
+        }
+    }
+}
